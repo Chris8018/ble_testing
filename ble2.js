e716481b0d18d4fe18ff1786ae98067898bc8b74
Chris8018 <tranuytrieuvi@gmail.com>
2019-03-07 17:48:51 +0000

    Update

--- ble2.js
+++ ble2.js
@@ -13,8 +13,12 @@ async function onScanButtonClick() {
         //let serviceUuid = 0x180a;
         console.log('Requesting Bluetooth Device...');
         const device = await navigator.bluetooth.requestDevice({
+            // filters: [
+            //     //{name: 'CC2650 SensorTag'},
+            //     {services: ['f000aa00-0451-4000-b000-000000000000']}
+            // ]
             acceptAllDevices: true,
-            //optionalServices: [{'device_information': 0x180a}]
+            //optionalServices: [0x180a]
         });
 
         let dName = await device.name;
@@ -26,7 +30,14 @@ async function onScanButtonClick() {
         console.log('ID: ' + dID);
         console.log('GATT: ' + dGATT);
         const server = await device.gatt.connect();
-        const service = await server.getPrimaryService('0000180a-0000-1000-8000-00805f9b34fb');
+        console.log("Connected! " + server.connected);
+        //const s1 = await server.getPrimaryServices(0x180a);
+        let s1 = await server.getPrimaryServices();
+        console.log("Got service!");
+        console.log(s1)
+        // const name = await service.getCharacteristic(0x2a24);
+        //console.log('Got characteristic Device_modelNum')
+        //console.log(name.properties.read);
         //console.log(server.getPrimaryServices());
         //console.log(device.uuids);
         // console.log('UUIDs');
---
+++ ble3.js
@@ -0,0 +1,50 @@
+function onScanButtonClick() {
+    let options = {
+        acceptAllDevices: true,
+        optionalServices: ['f000aa00-0451-4000-b000-000000000000']
+    };
+
+    navigator.bluetooth.requestDevice(options)
+        .then(device => {
+            return device.gatt.connect();
+        })
+        .then(server => {
+            console.log('Try to get services')
+            return server.getPrimaryService('f000aa00-0451-4000-b000-000000000000');
+        })
+        .then(service => {
+            console.log('Enable Temperature scanning');
+            service.getCharacteristic('f000aa02-0451-4000-b000-000000000000').then(charConfig => {
+                var value = new Uint8Array([0x01]);
+                charConfig.writeValue(value);
+            });
+
+            console.log('Retrieve Temperature Data');
+            service.getCharacteristic('f000aa01-0451-4000-b000-000000000000')
+            .then(charData => {
+                charData.startNotifications().then(_ => {
+                    charData.addEventListener('characteristicvaluechanged', handleTempChange);
+                });
+            });
+        })
+        .catch(error => {
+            console.log('Argh! ' + error);
+        });
+
+        function handleTempChange(event) {
+            let raw_data = event.target.value;
+            //console.log(raw_data);
+    
+            let temp1 = raw_data.getUint8(3).toString(16);
+            temp1 = temp1.length < 2 ? '0' + temp1 : temp1;
+    
+            let temp2 = raw_data.getUint8(2).toString(16);
+            temp2 = temp2.length < 2 ? '0' + temp2 : temp2;
+    
+            let raw_ambient_temp = parseInt('0x' + temp1 + temp2, 16);
+            let ambient_temp_int = raw_ambient_temp >> 2 & 0x3FFF;
+            let resultC = ambient_temp_int * 0.03125;
+
+            console.log('Temperature: ' + resultC);
+        }
+}
